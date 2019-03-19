332bd62e2e5797e6c1a1776230a21934daf8e62d
StableWeaver <ash8dbz@gmail.com>
2019-03-19 23:53:18 +0530

    Renamed files in Assignment3 Folder

--- Assignments/Assignment 3/Assignment1_4.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter no of elements"))
-arr=list()
-for i in range(n):
-    arr.append(int(input("Enter "+str(i+1)+" element? ")))
-    print()
-
-count=arr.count(int(input("Enter the no to be serached? :")))
-print("\nThe frequency of no is {}".format(count))
